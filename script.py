69a40e23345dd5e06b8685b10c933b76b20d1749
Alexandru Gutu <alex.s.gutu@gmail.com>
2016-12-10 00:07:16 -0500

    Initial, works for file list in folder

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ import imgurpython
 import os
 import cv2
 
-
-imgSources = "/resources/"
+imgSources = "./resources/"
+catCascade = "haarcascade_frontalcatface.xml"
 
 imgurClientId = "abb3bc18c3e725b"
 imgurClientSecret = "d2df58730cc8ed27ed6eb72ebca03aed259707ae"
@@ -12,14 +12,13 @@ imgurClientSecret = "d2df58730cc8ed27ed6eb72ebca03aed259707ae"
 client = imgurpython.ImgurClient(imgurClientId, imgurClientSecret)
 
 for filename in os.listdir(imgSources):
-    image = cv2.imread(filename)
+    image = cv2.imread(imgSources + filename)
     grayScaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # load the cat detector Haar cascade, then detect cat faces
     # in the input image
-    detector = cv2.CascadeClassifier(args["cascade"])
-    rectangleBoundaries = detector.detectMultiScale(grayScaleImage, scaleFactor=1.04, minNeighbors=20,
-                                                    minSize=(600, 600))
+    detector = cv2.CascadeClassifier("./" + catCascade)
+    rectangleBoundaries = detector.detectMultiScale(grayScaleImage, scaleFactor=1.3, minNeighbors=10, minSize=(75, 75))
 
     # loop over the cat faces and draw a rectangle surrounding each
     for (i, (x, y, w, h)) in enumerate(rectangleBoundaries):
@@ -28,10 +27,10 @@ for filename in os.listdir(imgSources):
                     cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
     # resive image to fit on screen
-    resizedImage = cv2.resize(image, (1280, 1024))
+    # resizedImage = cv2.resize(image, (1920, 1080))
 
     # show the detected cat faces
     # cv2.namedWindow("preview", cv2.WINDOW_NORMAL)
-    # cv2.imshow("Cat Faces", resizedImage)
-    # cv2.waitKey(0)
+    cv2.imshow("Cat Faces", image)
+    cv2.waitKey(0)
 
